23da5ec37239bb19cec30f839ef49aae217f6897
byrneg7 <byrneg7@tcd.ie>
2020-05-24 19:21:01 +0100

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://stormy-woodland-51514.herokuapp.com/"
+    origins "http://localhost:3000", "https://stormy-woodland-51514.herokuapp.com"
 
     resource "*",
              headers: :any,
